58670d50eb5d9c6c42125893a2ce2f335cce6b25
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2017-06-28 23:49:52 +0200

    Several decorators added.

---
+++ app/src/main/java/com/alekso/budget/model/decorators/ReviewAccount.java
@@ -0,0 +1,23 @@
+package com.alekso.budget.model.decorators;
+
+import com.alekso.budget.model.Account;
+
+/**
+ * Account data for Review screen
+ * <p>
+ * Created by alekso on 28/06/2017.
+ */
+public class ReviewAccount {
+    /**
+     * Account data
+     */
+    private Account mAccount;
+    /**
+     * Balance for the account at the current (system) time
+     */
+    private double mBalance;
+    /**
+     * Image to represent the account
+     */
+    private int mImageResource;
+}
--- app/src/main/java/com/alekso/budget/model/decorators/TimelineItem.java
+++ app/src/main/java/com/alekso/budget/model/decorators/TimelineItem.java
@@ -1,4 +1,6 @@
-package com.alekso.budget.model;
+package com.alekso.budget.model.decorators;
+
+import com.alekso.budget.model.Transaction;
 
 /**
  * Created by alekso on 14/05/2017.
