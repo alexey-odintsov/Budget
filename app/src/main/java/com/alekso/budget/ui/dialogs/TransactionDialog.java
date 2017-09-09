0ae8f0694ce23f87148535dc2269d252303ce015
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2017-09-09 23:42:52 +0300

    Create Transaction dialog frame. ConstraintLayout library updated.

--- app/src/main/java/com/alekso/budget/ui/dialogs/TransactionDialog.java
+++ app/src/main/java/com/alekso/budget/ui/dialogs/TransactionDialog.java
@@ -1,9 +1,14 @@
 package com.alekso.budget.ui.dialogs;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.alekso.budget.R;
 
 /**
  * Created by alekso on 27/08/2017.
@@ -16,6 +21,23 @@ public class TransactionDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_transaction, null))
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setTitle("Add new transaction");
+        return builder.create();
     }
+
 }
